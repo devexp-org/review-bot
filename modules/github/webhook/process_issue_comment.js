c997049b2c860a3a9d81f61491a901ab9aeb4023
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-06-08 19:01:47 +0300

    Start working on issue comment webhook processor

--- modules/github/webhook/process_issue_comment.js
+++ modules/github/webhook/process_issue_comment.js
@@ -1,3 +1,12 @@
-module.exports = function (github) {
+var getPrNumber = require('../utils/get_pr_number');
 
+module.exports = function (github) {
+    return function processIssueComment(body) {
+        var pullRequestTitle = body.issue.title,
+            pullRequestNumber = getPrNumber(body.issue.pull_request.url),
+            commentUser = body.comment.user.login,
+            repositoryName = body.repository.full_name;
+
+
+    };
 };
