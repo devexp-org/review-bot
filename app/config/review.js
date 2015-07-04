955489ed5f162e878d6e68cf22d6c6b280884c71
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-07-05 00:07:49 +0300

    snake_case everywhere

--- app/config/review.js
+++ app/config/review.js
@@ -3,20 +3,20 @@ import * as config from 'app/core/config';
 export default {
     approveCount: 2,
     processors: [
-        require('app/plugins/review-choose-reviewers/review-github-org-team')(config.load('github_org_team')),
-        require('app/plugins/review-choose-reviewers/review-remove-author')(),
-        require('app/plugins/review-choose-reviewers/review-remove-already-reviewers')(),
-        require('app/plugins/review-choose-reviewers/review-random')(15),
-        require('app/plugins/review-choose-reviewers/review-sort')(),
-        require('app/plugins/review-choose-reviewers/review-total-number')(2)
+        require('app/plugins/review_choose_reviewers/github_org_team')(config.load('github_org_team')),
+        require('app/plugins/review_choose_reviewers/remove_author')(),
+        require('app/plugins/review_choose_reviewers/remove_already_reviewers')(),
+        require('app/plugins/review_choose_reviewers/random')(15),
+        require('app/plugins/review_choose_reviewers/sort')(),
+        require('app/plugins/review_choose_reviewers/total_number')(2)
     ],
     listeners: {
-        'github:pull_request:opened': [require('app/plugins/review-autoassign')()],
+        'github:pull_request:opened': [require('app/plugins/review_autoassign')()],
         'github:issue_comment': [
-            require('app/plugins/review-commands/dispatcher')({
+            require('app/plugins/review_commands/dispatcher')({
                 commands: {
-                    'start': [require('app/plugins/review-commands/start')()],
-                    'ok': [require('app/plugins/review-commands/ok')()]
+                    'start': [require('app/plugins/review_commands/start')()],
+                    'ok': [require('app/plugins/review_commands/ok')()]
                 },
                 aliases: {}
             })
