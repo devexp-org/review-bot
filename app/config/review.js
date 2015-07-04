72cab9e6d52be3214f4c1b004b97dd5b2e3b1563
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-07-04 22:55:15 +0300

    Group review plugins

--- app/config/review.js
+++ app/config/review.js
@@ -3,12 +3,12 @@ import * as config from 'app/core/config';
 export default {
     approveCount: 2,
     reducers: [
-        require('app/plugins/review-github-org-team')(config.load('github_org_team')),
-        require('app/plugins/review-remove-author')(),
-        require('app/plugins/review-remove-already-reviewers')(),
-        require('app/plugins/review-random')(15),
-        require('app/plugins/review-sort')(),
-        require('app/plugins/review-total-number')(2)
+        require('app/plugins/review-choose-reviewers/review-github-org-team')(config.load('github_org_team')),
+        require('app/plugins/review-choose-reviewers/review-remove-author')(),
+        require('app/plugins/review-choose-reviewers/review-remove-already-reviewers')(),
+        require('app/plugins/review-choose-reviewers/review-random')(15),
+        require('app/plugins/review-choose-reviewers/review-sort')(),
+        require('app/plugins/review-choose-reviewers/review-total-number')(2)
     ],
     listeners: {
         'github:pull_request:opened': [require('app/plugins/review-autoassign')()],
