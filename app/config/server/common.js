544071d349ed80183904636a1cbd4ffeae8560bf
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-07-13 11:48:55 +0300

    Change PORT to NODE_PORT in server config

--- app/config/server/common.js
+++ app/config/server/common.js
@@ -3,5 +3,5 @@ var path = require('path');
 module.exports = {
     staticBase: '/public',
     staticPath: path.join(__dirname, '..', '..', '..', 'public'),
-    port: process.env.PORT || 3000
+    port: process.env.NODE_PORT || 3000
 };
