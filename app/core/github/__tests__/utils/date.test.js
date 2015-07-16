a92e4d6e6a9b215b498734434e5e0373af2dea91
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-07-16 16:29:32 +0300

    Github utils tests

---
+++ app/core/github/__tests__/utils/date.test.js
@@ -0,0 +1,13 @@
+describe('core/github/utils/date', function () {
+    describe('#getSinceDate', function () {
+        var getSinceDate = require('../../utils/date').getSinceDate;
+
+        it('should return nothing if date is empty', function () {
+            assert.notOk(getSinceDate());
+        });
+
+        it('should return formated date', function () {
+            assert.ok(getSinceDate([1, 'day']));
+        });
+    });
+});
