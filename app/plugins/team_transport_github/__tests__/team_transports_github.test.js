313b7feac5ee4722b7f3b9dbe25ad078de2f6ae9
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-07-18 21:54:46 +0300

    Github transport for team module

---
+++ app/plugins/team_transport_github/__tests__/team_transports_github.test.js
@@ -0,0 +1,59 @@
+describe('app/plugins/team_transport_github', function () {
+    var githubApi = { api: {} };
+    var proxyquire = require('proxyquire');
+    var transport;
+    var team;
+    var org;
+
+    beforeEach(function () {
+        team = [1, 2, 3, 4];
+        org = [5, 6, 7, 7];
+
+        githubApi.api.orgs = {
+            getTeams: function (opts, cb) {
+                cb(null, [{ slug: 'team1', id: 123 }]);
+            },
+            getTeamMembers: function (opts, cb) {
+                cb(null, team);
+            },
+            getMembers: function (opts, cb) {
+                cb(null, org);
+            }
+        };
+
+        transport = proxyquire('../index.js', {
+            'app/core/github/api': githubApi
+        });
+    });
+
+    it('should rejected if there is no params passed', function () {
+        assert.isRejected(transport());
+        assert.isRejected(transport({}));
+    });
+
+    describe('no team passed', function () {
+        it('should get all org members if team not specified', function (done) {
+            transport({ org: 'org' })
+                .then(function (t) {
+                    assert.equal(t, org);
+
+                    done();
+                }, done);
+        });
+    });
+
+    describe('if team passed', function () {
+        it('should get team from github organisation team', function (done) {
+            transport({ org: 'org', team: 'team1' })
+                .then(function (t) {
+                    assert.equal(t, team);
+
+                    done();
+                }, done);
+        });
+
+        it('should be rejected if there is no such team in github organisation', function () {
+            assert.isRejected(transport({ org: 'org', team: 'team2' }));
+        });
+    });
+});
