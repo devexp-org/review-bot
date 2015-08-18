c325d4b4649597a60e7db3eeed445503476bb019
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-08-18 13:17:24 +0300

    Add secret.json template

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -5,7 +5,13 @@ source ./colors.sh 2>/dev/null;
 NODE_MODULES=node_modules
 
 if [ ! -d $NODE_MODULES/client ]; then
+    echo "Creating symlink to './client' folder"
     ln -s ../client $NODE_MODULES/client
+fi
+
+if [ ! -f config/secret.json ]; then
+    echo "Copying empty secret.json"
+    cp scripts/templates/secret.json config/
 fi
 
 mkdir -p data 2> /dev/null
