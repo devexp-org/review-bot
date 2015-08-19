545925599d8fe180a18c11fd568d795c54bd10cc
Artem Semenov <xxxxxx@yandex-team.ru>
2015-08-19 09:55:15 +0300

    Remove `badge-constructor` service

--- services/badge-constructor/routes.js
+++ services/badge-constructor/routes.js
@@ -2,10 +2,12 @@
 
 import middleware from 'badgs/lib/middleware';
 
+import Badgs from 'badgs';
 import { Router as router } from 'express';
 
 export default function badgeRouterCreator(options, imports) {
-  const badge = imports['badge-constructor'];
+
+  const badge = new Badgs(options.template);
   const badgeRouter = router();
 
   badgeRouter.use(middleware(badge));
