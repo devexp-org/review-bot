9a7411b53c54b3f6dd9d28fa36d1cf5b9173e4bd
Artem Semenov <xxxxxx@yandex-team.ru>
2015-08-21 17:10:48 +0300

    Application fully covered with tests

--- modules/application/__tests__/index.js
+++ modules/application/__tests__/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import path from 'path';
 import Application from '../../application';
 
 // TODO when all ignored
@@ -50,7 +51,7 @@ describe('modules/application', function () {
               setTimeout(function () { resolve({ service: 'moduleA' }); }, 10);
             });
           },
-          dependencies: ['serviceB', 'serviceC']
+          dependencies: ['serviceB', 'serviceC', 'serviceD']
         },
         serviceB: {
           module: function () {
@@ -65,13 +66,29 @@ describe('modules/application', function () {
               setTimeout(function () { resolve({ service: 'moduleC' }); }, 20);
             });
           }
+        },
+        serviceD: {
+          module: function () {
+            return Promise.resolve({ service: 'moduleD' });
+          }
         }
       }
     };
 
     app = new Application(config);
 
-    app.execute().then(() => done()).catch(done);
+    app
+      .execute()
+      .then(resolved => {
+        assert.deepEqual(resolved, {
+          serviceA: 'moduleA',
+          serviceB: 'moduleB',
+          serviceC: 'moduleC',
+          serviceD: 'moduleD'
+        });
+        done();
+      })
+      .catch(done);
 
   });
 
@@ -163,6 +180,111 @@ describe('modules/application', function () {
       });
   });
 
+  it('should skip ignored services', function (done) {
+    const order = [];
+
+    config = {
+      services: {
+        serviceA: {
+          module: function () {
+            order.push('serviceA');
+            return Promise.resolve({ service: 'moduleA' });
+          }
+        },
+        serviceB: {
+          ignore: true,
+          module: function () {
+            order.push('serviceB');
+            return Promise.resolve({ service: 'moduleB' });
+          }
+        }
+      }
+    };
+
+    app = new Application(config);
+
+    app
+      .execute()
+      .then(function (resolved) {
+        assert.deepEqual(order, ['serviceA']);
+        assert.deepEqual(resolved, { serviceA: 'moduleA' });
+        done();
+      })
+      .catch(done);
+
+  });
+
+  it('should reject promise if error occurs in service requiring', function (done) {
+    config = {
+      services: {
+        serviceA: {
+          path: 'path/not/exists'
+        }
+      }
+    };
+
+    app = new Application(config);
+
+    app
+      .execute()
+      .catch(e => {
+        assert.instanceOf(e, Error);
+        assert.match(e.message, /path\/not\/exists/i);
+        done();
+      });
+
+  });
+
+  it('should reject promise if error occurs in service startup', function (done) {
+    config = {
+      services: {
+        serviceA: {
+          module: function () {
+            throw new Error('Error in serviceA');
+          }
+        }
+      }
+    };
+
+    app = new Application(config);
+
+    app
+      .execute()
+      .catch(e => {
+        assert.instanceOf(e, Error);
+        assert.match(e.message, /Error in serviceA/i);
+        done();
+      });
+
+  });
+
+  it('should throw an error if startup of module timeouted', function (done) {
+    config = {
+      startup_timeout: 10,
+      services: {
+        serviceA: {
+          module: function () {
+            return new Promise(resolve => {
+              setTimeout(function () {
+                resolve({ service: 'serviceA' });
+              }, 20);
+            });
+          }
+        }
+      }
+    };
+
+    app = new Application(config);
+    app
+      .execute()
+      .then(done)
+      .catch(e => {
+        assert.instanceOf(e, Error);
+        assert.match(e.message, /timeout/i);
+        done();
+      });
+  });
+
   it('should not allow to execute an application twice', function () {
     app = new Application();
 
@@ -172,6 +294,242 @@ describe('modules/application', function () {
       app.execute.bind(app),
       /cannot execute the application twice/i
     );
+  });
+
+  describe('#values', function () {
+
+    it('should return array of object values without keys', function () {
+      const test = { a: 'aa', b: 'bb' };
+
+      assert.deepEqual(Application.values(test), ['aa', 'bb']);
+    });
+
+  });
+
+  describe('#shutdown', function () {
+
+    it('should gracefuly shutdown services', function (done) {
+      const order = [];
+
+      config = {
+        services: {
+          serviceA: {
+            module: function () {
+              order.push('start serviceA');
+              return Promise.resolve({
+                service: 'moduleA',
+                shutdown: function () {
+                  order.push('shutdown serviceA');
+                  return Promise.resolve();
+                }
+              });
+            },
+            dependencies: ['serviceB']
+          },
+          serviceB: {
+            module: function () {
+              order.push('start serviceB');
+              return Promise.resolve({
+                service: 'moduleB',
+                shutdown: function () {
+                  order.push('shutdown serviceB');
+                  return Promise.resolve();
+                }
+              });
+            }
+          }
+        }
+      };
+
+      app = new Application(config);
+
+      app
+        .execute()
+        .then(::app.shutdown)
+        .then(() => {
+          assert.deepEqual(order, [
+            'start serviceB',
+            'start serviceA',
+            'shutdown serviceB',
+            'shutdown serviceA'
+          ]);
+          done();
+        })
+        .catch(done);
+    });
+
+    it('should throw an error if app is not fully started', function () {
+      app = new Application({});
+
+      try {
+        app.shutdown();
+        assert.fail('it should fail');
+      } catch (e) {
+        assert.instanceOf(e, Error);
+        assert.match(e.message, /started/i);
+      }
+    });
+
+    it('should throw an error if shutdown function timeouted', function (done) {
+      config = {
+        shutdown_timeout: 10,
+        services: {
+          serviceA: {
+            module: function () {
+              return Promise.resolve({
+                service: 'moduleA',
+                shutdown: function () {
+                  return new Promise(resolve => {
+                    setTimeout(function () { resolve({ service: 'serviceA' }); }, 20);
+                  });
+                }
+              });
+            }
+          },
+          serviceB: {
+            module: function () {
+              return Promise.resolve({ service: 'serviceB' });
+            }
+          }
+        }
+      };
+
+      app = new Application(config);
+      app
+        .execute()
+        .then(::app.shutdown)
+        .then(done)
+        .catch(e => {
+          assert.instanceOf(e, Error);
+          assert.match(e.message, /timeout/i);
+          done();
+        });
+    });
+
+  });
+
+  describe('require', function (done) {
+
+    it('`require` is is forbidden service name', function () {
+      config = {
+        services: {
+          require: {
+            module: function () {
+              return Promise.resolve({ service: 'service' });
+            }
+          }
+        }
+      };
+
+      app = new Application(config);
+
+      app
+        .execute()
+        .catch(e => {
+          assert.instanceOf(e, Error);
+          assert.match(e.message, /is forbidden/i);
+          done();
+        });
+    });
+
+    it('`requireDefault` is is forbidden service name', function () {
+      config = {
+        services: {
+          requireDefault: {
+            module: function () {
+              return Promise.resolve({ service: 'service' });
+            }
+          }
+        }
+      };
+
+      app = new Application(config);
+
+      app
+        .execute()
+        .catch(e => {
+          assert.instanceOf(e, Error);
+          assert.match(e.message, /is forbidden/i);
+          done();
+        });
+    });
+
+    it('`require` is is forbidden alias', function () {
+      config = {
+        services: {
+          serviceA: {
+            module: function () {
+              return Promise.resolve({ service: 'serviceA' });
+            }
+          },
+          serviceB: {
+            module: function () {
+              return Promise.resolve({ service: 'serviceB' });
+            },
+            dependencies: { require: 'serviceA' }
+          }
+        }
+      };
+
+      app = new Application(config);
+
+      app
+        .execute()
+        .catch(e => {
+          assert.instanceOf(e, Error);
+          assert.match(e.message, /is forbidden/i);
+          done();
+        });
+    });
+
+    it('`require` is is forbidden alias', function () {
+      config = {
+        services: {
+          serviceA: {
+            module: function () {
+              return Promise.resolve({ service: 'serviceA' });
+            }
+          },
+          serviceB: {
+            module: function () {
+              return Promise.resolve({ service: 'serviceB' });
+            },
+            dependencies: { requireDefault: 'serviceA' }
+          }
+        }
+      };
+
+      app = new Application(config);
+
+      app
+        .execute()
+        .catch(e => {
+          assert.instanceOf(e, Error);
+          assert.match(e.message, /is forbidden/i);
+          done();
+        });
+    });
+
+    it('#require should join path with `basePath`', function () {
+      app = new Application({}, path.join(__dirname, 'mocks'));
+
+      assert.deepEqual(app.require('./test'), './mocks/test.js');
+    });
+
+    it('#require should not join path with `basePath` if path starts with `/`', function () {
+      app = new Application({}, path.join(__dirname, 'mocks'));
+      const absolutePath = path.join(__dirname, 'mocks', 'test.js');
+
+      assert.deepEqual(app.require(absolutePath), './mocks/test.js');
+    });
+
+    it('#requireDefault should return default export', function () {
+      app = new Application({}, path.join(__dirname, 'mocks'));
+
+      assert.deepEqual(app.requireDefault('./test'), './mocks/test.js');
+      assert.deepEqual(app.requireDefault('./es6.js'), './mocks/es6.js');
+    });
+
   });
 
 });
---
+++ modules/application/__tests__/mocks/test.js
@@ -0,0 +1,1 @@
+module.exports = './mocks/test.js';
