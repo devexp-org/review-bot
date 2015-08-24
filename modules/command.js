1552477a6c6746e0969948ad79239ac5fd8af456
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-08-24 22:10:17 +0300

    Commands queue

--- modules/command.js
+++ modules/command.js
@@ -1,8 +1,8 @@
 'use strict';
 
-import _ from 'lodash';
+import { forEach, isEmpty } from 'lodash';
 
-export default class CommandDistatcher {
+export default class CommandDispatcher {
 
   /**
    * @constructor
@@ -11,6 +11,62 @@ export default class CommandDistatcher {
    */
   constructor(store) {
     this.store = store || [];
+    this.queue = {};
+  }
+
+  /**
+   * Add command to queue.
+   *
+   * @param {Function} handler
+   * @param {String} commentCommand
+   * @param {Object} payload
+   *
+   * @return {Promise}
+   */
+  enqueue(handler, commentCommand, payload) {
+    const pullId = payload.pullRequest.id;
+
+    return new Promise((resolve, reject) => {
+      this.queue[pullId] = this.queue[pullId] || { active: false, queue: [] };
+      this.queue[pullId].queue.push({ handler, commentCommand, payload, resolve, reject });
+    });
+  }
+
+  /**
+   * Queue step.
+   *
+   * @param {Object} pullRequest
+   */
+  step(pullRequest) {
+    const pullId = pullRequest.id;
+
+    if (!this.queue[pullId] || this.queue[pullId].active) {
+      return;
+    }
+
+    const item = this.queue[pullId];
+    const itemQueueStep = item.queue.shift();
+
+    item.active = true;
+    itemQueueStep.payload.pullRequest = pullRequest;
+
+    itemQueueStep
+      .handler(itemQueueStep.commentCommand, itemQueueStep.payload)
+      .catch(err => {
+        itemQueueStep.reject(err);
+
+        return pullRequest;
+      })
+      .then(pullRequestUpdated => {
+        item.active = false;
+        itemQueueStep.resolve();
+
+        if (isEmpty(item.queue)) {
+          this.queue[pullId] = null;
+        }
+
+        this.step(pullRequestUpdated);
+      });
   }
 
   /**
@@ -22,20 +78,23 @@ export default class CommandDistatcher {
    * @return {Promise}
    */
   dispatch(comment, payload) {
-    const promise = [];
+    const promiseList = [];
 
-    _.forEach(this.store, command => {
-      _.forEach(comment.split('\n'), line => {
+    forEach(this.store, command => {
+      forEach(comment.split('\n'), line => {
         if (command.test.test(line)) {
-          _.forEach(command.handlers, handler => {
+          forEach(command.handlers, handler => {
             const commentCommand = line.trim().toLowerCase();
-            promise.push(handler(commentCommand, payload));
+
+            promiseList.push(this.enqueue(handler, commentCommand, payload));
           });
         }
       });
     });
 
-    return Promise.all(promise).then(() => {});
+    this.step(payload.pullRequest);
+
+    return Promise.all(promiseList);
   }
 
 }
