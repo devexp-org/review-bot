390d00df43db71e2e4537f6087c0aa9cd46951fe
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-10-25 18:32:05 +0300

    test(badges): Tests for BaseBadge class

---
+++ modules/badge-base/__tests__/index.js
@@ -0,0 +1,29 @@
+import BadgeBase from '../../badge-base';
+
+describe('modules/badge-base', () => {
+  const badgeBase = new BadgeBase('http://shields.io/badges/');
+
+  describe('#create', () => {
+    it('should throw an error if there is no subject', () => {
+      assert.throws(() => badgeBase.create());
+    });
+
+    it('should throw an error if there is no status', () => {
+      assert.throws(() => badgeBase.create('subject'));
+    });
+
+    it('should build <img> tag with correct url', () => {
+      assert.include(badgeBase.create('status', 'subject', 'green'), 'http://shields.io/badges/status-subject-green.svg');
+    });
+
+    it('should wrap badge in <a> tag if url is passed', () => {
+      assert.include(badgeBase.create('status', 'subject', 'green', 'https://github.com'), '<a');
+    });
+  });
+
+  describe('#_escape', () => {
+    it('should replace - to --', () => assert.equal(badgeBase._escape('sta-tus'), 'sta--tus'));
+    it('should replace space to _', () => assert.equal(badgeBase._escape('sta tus'), 'sta_tus'));
+    it('should replace _ to __', () => assert.equal(badgeBase._escape('sta_tus'), 'sta__tus'));
+  });
+});
