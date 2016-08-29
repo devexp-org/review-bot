b2b34c35f6386696f6bacaa9df148b4ed2143c8b
Artem Semenov <xxxxxx@yandex-team.ru>
2016-08-29 13:05:21 +0300

    Add plugin manager

--- src/modules/config/index.js
+++ src/modules/config/index.js
@@ -50,8 +50,6 @@ export function transform(basePath, json) {
             addons.push(visit(includeContent));
 
             return null;
-          } else if (k.substr(0, 0) === '#plugin:') {
-            return null;
           } else {
             return visit(v);
           }
---
+++ src/services/plugin-manager/index.js
@@ -0,0 +1,36 @@
+import { forEach } from 'lodash';
+
+export function findPlugins(config) {
+  const plugins = [];
+
+  forEach(config.services, (service, serviceName) => {
+
+    forEach(Object.keys(service), (key) => {
+
+      if (key.substr(0, 8) === '#plugin:') {
+        plugins.push({
+          service: key.substr(8),
+          options: service[key],
+          dependency: serviceName
+        });
+      }
+
+    });
+
+  });
+
+  return plugins;
+
+}
+
+export default function setup(options, imports) {
+
+  const app = imports.__app__;
+  const plugins = findPlugins(app.getConfig());
+
+  forEach(plugins, (plugin) => {
+    app.addOptions(plugin.service, plugin.options);
+    app.addDependency(plugin.service, plugin.dependency);
+  });
+
+}
