4115517096688f7595b661d60bacfef4397cc641
Artem Semenov <xxxxxx@yandex-team.ru>
2016-09-09 19:08:48 +0300

    wip

--- client/app/containers/Home.js
+++ client/app/containers/Home.js
@@ -2,6 +2,7 @@ import React, { PropTypes, Component } from 'react';
 import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
 import * as UsersActions from '../actions/users';
+import * as UserFormActions from '../actions/user-form';
 import UserList from '../components/UserList';
 
 // @connect(state => { users: state.users })
@@ -33,12 +34,20 @@ class Home extends Component {
   }
 
   render() {
+    var form = this.props.userForm;
+    const handleChange = (name) => (event) => this.props.handleChange(name, event);
+
     return (
       <div>
         <Helmet title="Home" />
         <h4>New user</h4>
-        <form onSubmit={this.handleSubmit}>
-          <input name="login" size="25" />
+        <form onSubmit={this.props.handleSubmit}>
+          <input
+            size="25"
+            name="login"
+            value={form.login && form.login.value || ''}
+            onChange={handleChange('login')}
+          />
           <button type="submit">Add</button>
         </form>
         <h5>Users:</h5>
@@ -48,15 +57,32 @@ class Home extends Component {
   }
 }
 
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+
+    handleChange: (field, event) => {
+      dispatch({ type: USER_FORM_CHANGE, field, value: event.target.value })
+    },
+
+    handleSubmit: (event) => {
+      event.preventDefault();
+      dispatch({ type: UserFormActions.USER_FORM_SUBMIT, form })
+    }
+  }
+}
+
 function mapStateToProps(state) {
   return {
+    userForm: state.userForm,
     users: state.users
   };
 }
 
 Home.propTypes = {
   users: PropTypes.object.isRequired,
+  userForm: PropTypes.object.isRequired,
   dispatch: PropTypes.func.isRequired
 };
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- client/app/reducers/index.js
+++ client/app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import userForm from './user-form';
 import users from './users';
 import user from './user';
 
 export default combineReducers({
+  userForm,
   users,
   user
 });
