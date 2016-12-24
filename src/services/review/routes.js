64a9eb3a990a6fd58c03ebdc7003aff9cd93bfaa
Artem Semenov <xxxxxx@yandex-team.ru>
2016-12-24 23:07:18 +0300

    wip

---
+++ src/services/review/routes.js
@@ -0,0 +1,22 @@
+import { forEach } from 'lodash';
+import { Router as router } from 'express';
+
+export default function setup(options, imports) {
+
+  const review = imports.review;
+
+  const reviewRoute = router();
+
+  reviewRoute.get('/steps', function (req, res) {
+    const steps = {};
+
+    forEach(review.getSteps(), (step) => {
+      steps[step.name()] = step.config();
+    });
+
+    res.json(steps);
+  });
+
+  return reviewRoute;
+
+}
