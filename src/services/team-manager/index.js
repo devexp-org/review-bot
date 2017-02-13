35bcce23744543d343ac31a642af5f0c95946545
Artem Semenov <xxxxxx@yandex-team.ru>
2017-02-13 18:19:19 +0300

    fix user search

--- src/services/team-manager/index.js
+++ src/services/team-manager/index.js
@@ -3,7 +3,7 @@ import TeamManager from './class';
 export default function setup(options, imports) {
 
   const model = imports.model;
-  const search = imports.search;
+  const search = imports['team-search'];
 
   return new TeamManager(model('team'), search);
 
